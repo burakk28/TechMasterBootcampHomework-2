5311852350d511e9ff81ab3d129d5ee58865318d
Burak Kara <karaa.burak28@gmail.com>
2023-01-07 19:22:52 +0300

    Update Contents.swift

--- homework2.playground/Contents.swift
+++ homework2.playground/Contents.swift
@@ -88,7 +88,7 @@ class Homework2 {
 var homework2=Homework2()
 
 //Verilen dereceyi Fahrenheit'a cevirir.
-//Örnek Değer : 23
+
 var result1 = homework2.celsiusToFahrenheit(number: 23)
 print("23 C = \(result1) F")
 
@@ -98,7 +98,7 @@ homework2.rectangularPerimeter(short: 14, long: 8)
 
 
 //Faktçriyeli hesaplar.
-//Örnek Değer: 4
+
 var result3 = homework2.factorialCalculation(number:4)
 print("4! = \(result3)")
 
@@ -107,19 +107,19 @@ homework2.AFind(word: "BurakKara")
 
 
 //Kenar sayısına göre iç açılarını hesaplar.
-//Örnek Değer : 3
+
 var result5 = homework2.calculatingInteriorAngles(number: 3)
 print("Interior Angles = \(result5)")
 
 
 //Gün sayısına göre maaş hesaplar
-//Örnek Değer : 30
+
 var result6 = homework2.wageCalculator(day: 30)
 print("Wage : \(result6)")
 
 
 //Kota miktarına göre ücret hesaplar.
-//Örnek Değer : 60
+
 var result7 = homework2.internetPay(quota: 60)
 print("Pay : \(result7)")
 
